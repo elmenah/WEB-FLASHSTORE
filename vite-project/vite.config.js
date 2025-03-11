de5183d1fd01a6eaebc04e334e3c3a3bf88813bb
elmenah <ni.menag@duocuc.cl>
2025-03-11 19:29:55 -0300

    dist

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     outDir: 'dist', // Carpeta de salida
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'src/index.html'),
+        index: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'src/login.html'),
         shop: resolve(__dirname, 'src/shop.html'),
         registro: resolve(__dirname, 'src/registro.html'),
