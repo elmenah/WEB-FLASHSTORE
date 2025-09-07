9879a72eca26a2f4e9e372874ad21fe0dda13515
elmenah <ni.menag@duocuc.cl>
2025-09-07 01:55:06 -0300

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,6 +7,7 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
+  const [session, setSession] = useState(null); // Estado para la sesión activa
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -16,14 +17,11 @@ const Login = () => {
         console.error('Error al obtener sesión:', error.message);
         return;
       }
-      if (data.session) {
-        // Si hay una sesión activa, redirige a /home
-        navigate('/home');
-      }
+      setSession(data.session); // Actualiza el estado de la sesión
     };
 
     checkSession();
-  }, [navigate]);
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -39,6 +37,7 @@ const Login = () => {
       if (error) {
         setError('Credenciales incorrectas. Intenta nuevamente.');
       } else if (data.session) {
+        setSession(data.session); // Actualiza la sesión después del login
         navigate('/home'); // Redirige al usuario a /home después del login exitoso
       }
     } catch (err) {
@@ -70,6 +69,10 @@ const Login = () => {
     }
   };
 
+  const handleAccountRedirect = () => {
+    navigate('/micuenta'); // Redirige a la página MiCuenta
+  };
+
   return (
     <div className="h-screen flex items-center justify-center bg-gray-900 pt-14">
       <div className="w-full max-w-4xl flex shadow-lg rounded-lg overflow-hidden">
@@ -84,63 +87,73 @@ const Login = () => {
         <div className="w-full md:w-1/2 bg-gray-800 p-8 flex flex-col justify-center">
           <div className="w-full max-w-md bg-gray-800 p-6">
             <h2 className="text-2xl font-bold text-gray-200 mb-4">Login</h2>
-            <form className="flex flex-col" onSubmit={handleSubmit}>
-              <input 
-                type="email"
-                placeholder="Correo"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
-                required
-              />
-              <input 
-                type="password"
-                placeholder="Clave"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
-                required
-              />
-              
-              <div className="flex items-center justify-between flex-wrap">
-                <label className="text-sm text-gray-200 cursor-pointer">
-                  <input className="mr-2" type="checkbox" />
-                  Recuérdame
-                </label>
-                <a className="text-sm text-blue-500 hover:underline mb-0.5" href="#">
-                  ¿Olvidaste tu clave?
-                </a>
-              </div>
+            {!session ? (
+              <form className="flex flex-col" onSubmit={handleSubmit}>
+                <input 
+                  type="email"
+                  placeholder="Correo"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+                  required
+                />
+                <input 
+                  type="password"
+                  placeholder="Clave"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+                  required
+                />
+                
+                <div className="flex items-center justify-between flex-wrap">
+                  <label className="text-sm text-gray-200 cursor-pointer">
+                    <input className="mr-2" type="checkbox" />
+                    Recuérdame
+                  </label>
+                  <a className="text-sm text-blue-500 hover:underline mb-0.5" href="#">
+                    ¿Olvidaste tu clave?
+                  </a>
+                </div>
 
-              <p className="text-white mt-4">
-                ¿No tienes una cuenta? 
-                <Link className="text-sm text-blue-500 hover:underline ml-1" to="/register">
-                  Regístrate
-                </Link>
-              </p>
-              
-              <button
-                type="submit"
-                disabled={loading}
-                className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150 disabled:opacity-50"
-              >
-                {loading ? 'Iniciando sesión...' : 'Iniciar Sesión'}
-              </button>
+                <p className="text-white mt-4">
+                  ¿No tienes una cuenta? 
+                  <Link className="text-sm text-blue-500 hover:underline ml-1" to="/register">
+                    Regístrate
+                  </Link>
+                </p>
+                
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150 disabled:opacity-50"
+                >
+                  {loading ? 'Iniciando sesión...' : 'Iniciar Sesión'}
+                </button>
 
+                <button
+                  type="button"
+                  onClick={handleGoogleLogin}
+                  disabled={loading}
+                  className="bg-white text-gray-800 font-bold py-2 px-4 rounded-md mt-4 hover:bg-gray-200 transition ease-in-out duration-150 flex items-center justify-center border border-gray-300 shadow-md disabled:opacity-50"
+                >
+                  <img 
+                    src="https://img.icons8.com/?size=100&id=V5cGWnc9R4xj&format=png&color=000000" 
+                    alt="Google"
+                    className="w-5 h-5 mr-2"
+                  />
+                  Iniciar sesión con Google
+                </button>
+              </form>
+            ) : (
               <button
                 type="button"
-                onClick={handleGoogleLogin}
-                disabled={loading}
-                className="bg-white text-gray-800 font-bold py-2 px-4 rounded-md mt-4 hover:bg-gray-200 transition ease-in-out duration-150 flex items-center justify-center border border-gray-300 shadow-md disabled:opacity-50"
+                onClick={handleAccountRedirect}
+                className="bg-gradient-to-r from-green-500 to-teal-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-green-600 hover:to-teal-600 transition ease-in-out duration-150"
               >
-                <img 
-                  src="https://img.icons8.com/?size=100&id=V5cGWnc9R4xj&format=png&color=000000" 
-                  alt="Google"
-                  className="w-5 h-5 mr-2"
-                />
-                Iniciar sesión con Google
+                Mi Cuenta
               </button>
-            </form>
+            )}
             {error && <p className="text-red-400 text-sm mt-4">{error}</p>}
           </div>
         </div>
