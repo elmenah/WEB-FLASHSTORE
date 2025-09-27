94f73285b7aac2f8e221e01cf8254931f970ffae
elmenah <ni.menag@duocuc.cl>
2025-09-27 13:14:04 -0300

    A

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -43,7 +43,7 @@ export const CartProvider = ({ children }) => {
     ]);
   };
 
-  addToCart(testProduct);
+  
 
   const removeFromCart = (index) => {
     setCart(prevCart => prevCart.filter((_, i) => i !== index));
